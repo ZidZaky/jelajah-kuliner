67ecc16a34239495fd4fe9e3e969876d4c77239f
lalalalostyou <enjinadisellyn02@gmail.com>
2024-06-04 15:23:18 +0700

    ev up

---
+++ database/migrations/2024_05_27_181311_update_history.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('history_stoks', function (Blueprint $table) {
+            $table->integer('statusIsi')->default(0);
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('history_stoks', function (Blueprint $table) {
+            //
+        });
+    }
+};
