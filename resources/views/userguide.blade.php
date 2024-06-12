dcfdbb30d9307989b9334112d7844c9eb8b481d2
Farhan Nugraha <magmafarhan74@Gmail.com>
2024-06-12 17:37:48 +0700

    user guides

--- resources/views/userguide.blade.php
+++ resources/views/userguide.blade.php
@@ -9,18 +9,40 @@
 @endsection
 
 @section('main')
-    <div class="content">
-        <h1>User Guide Admin</h1>
-        <iframe allowfullscreen="allowfullscreen" scrolling="no" class="fp-iframe"
-            src="https://heyzine.com/flip-book/d2f8e85f02.html"
-            style="border: 1px solid lightgray; width: 100%; height: 400px;"></iframe>
-        <hr>
-        <h1>User Guide Visitor</h1>
-        <iframe allowfullscreen="allowfullscreen" scrolling="no" class="fp-iframe"
-            src="https://heyzine.com/flip-book/00bf2a3947.html"
-            style="border: 1px solid lightgray; width: 100%; height: 400px;"></iframe>
-            <hr>
-            <h1></h1>
+    <div class="content" style="margin: 1.5%;">
+        <div style="display: flex; justify-content: space-between;">
+            {{-- admin --}}
+            <div style="width: 48%; display: flex; flex-direction: column; align-items: center;">
+                <h1>User Guide Admin</h1>
+                <iframe allowfullscreen="allowfullscreen" scrolling="no" class="fp-iframe"
+                    src="https://heyzine.com/flip-book/d2f8e85f02.html"
+                    style="border: 1px solid lightgray; width: 100%; height: 550px;"></iframe>
+            </div>
+            {{-- guest --}}
+            <div style="width: 48%; display: flex; flex-direction: column; align-items: center;">
+                <h1>User Guide Visitor</h1>
+                <iframe allowfullscreen="allowfullscreen" scrolling="no" class="fp-iframe"
+                    src="https://heyzine.com/flip-book/00bf2a3947.html"
+                    style="border: 1px solid lightgray; width: 100%; height: 550px;"></iframe>
+            </div>
+        </div>
+        
+        <div style="display: flex; justify-content: space-between; margin-top: 3%; margin-bottom: 3%">
+            {{-- PKL --}}
+            <div style="width: 48%; display: flex; flex-direction: column; align-items: center;">
+                <h1>User Guide Admin</h1>
+                <iframe allowfullscreen="allowfullscreen" scrolling="no" class="fp-iframe"
+                    src="https://heyzine.com/flip-book/d2f8e85f02.html"
+                    style="border: 1px solid lightgray; width: 100%; height: 550px;"></iframe>
+            </div>
+            {{-- Customer --}}
+            <div style="width: 48%; display: flex; flex-direction: column; align-items: center;">
+                <h1>User Guide Visitor</h1>
+                <iframe allowfullscreen="allowfullscreen" scrolling="no" class="fp-iframe"
+                    src="https://heyzine.com/flip-book/00bf2a3947.html"
+                    style="border: 1px solid lightgray; width: 100%; height: 550px;"></iframe>
+            </div>
+        </div>
     </div>
 
 @endsection
